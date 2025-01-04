3dbc34fac1f040f6614446eba4ecdbe5e5817a00
teju1020 <tejasvirana3@gmail.com>
2025-01-04 18:09:33 +0530

    Initial commit

---
+++ books.js
@@ -0,0 +1,6 @@
+document.querySelector('.col-md-6.col-lg-3').addEventListener('click', function() {
+    window.location.href = 'endgame.html';
+  });
+document.querySelector('col-md-6 col-lg-4').addEventListener('click', function() {
+    window.location.href = 'box15.html';
+  });
---
+++ front.js
@@ -0,0 +1,16 @@
+alert("hello! Welcome to Book Tingle")
+document.getElementById('box11').addEventListener('click', function() {
+    window.location.href = 'box11.html'; 
+});
+document.getElementById('box12').addEventListener('click', function() {
+    window.location.href = 'box12.html'; 
+});
+document.getElementById('box13').addEventListener('click', function() {
+    window.location.href = 'box13.html'; 
+});
+document.getElementById('box14').addEventListener('click', function() {
+    window.location.href = 'box14.html'; 
+});
+document.getElementById('box15').addEventListener('click', function() {
+    window.location.href = 'box15.html'; 
+});
